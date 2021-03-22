06ee3a7ac9b36401b74f6133e295a593de2919f0
SamuelHGerber <henrylightfoot@gmail.com>
2021-03-22 14:43:22 -0700

    capitalizeAndFilter test written and failing

---
+++ functions/cap-and-filter.js
@@ -0,0 +1,13 @@
+const capitalizeAndFilter = () => {};
+
+module.exports = capitalizeAndFilter;
+
+//   ## capitalizeAndFilter
+
+// create a function that takes an array of strings capitalize all strings and filter
+// out any string that starts with the letter f.
+
+// 1. **Write a test**
+// 1. create a function, use a for loop to get test to pass
+// 1. green, commit
+// 1. refactor, use array methods to instead of a for loop
---
+++ functions/cap-and-filter.test.js
@@ -0,0 +1,13 @@
+const capitalizeAndFilter = require('./cap-and-filter');
+
+describe('capitalize and filter function', () => {
+  it('takes in an array of strings and capitalizes all but filters out ones that start with the letter F or f', () => {
+    const stringArray = ['helga', 'Norway', 'fjord', 'vikiNG', 'axe'];
+
+    const capitalizedArray = capitalizeAndFilter(stringArray);
+
+    const expectedArray = ['HELGA', 'NORWAY', 'VIKING', 'AXE'];
+
+    expect(capitalizedArray).toEqual(expectedArray);
+  });
+});
