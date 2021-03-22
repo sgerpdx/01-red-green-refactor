4fd2346c98ef71057582dba8aa1be36a92eb2cf7
SamuelHGerber <henrylightfoot@gmail.com>
2021-03-22 14:30:43 -0700

    both tests passing for copyAndPush

--- functions/copy-and-push.js
+++ functions/copy-and-push.js
@@ -1,5 +1,8 @@
-const copyAndPush = () => {
-  return [1, 2, 3, 4];
+const copyAndPush = (array, item) => {
+  const copiedArray = [...array];
+  copiedArray.push(item);
+  return copiedArray;
+
 };
 
 module.exports = copyAndPush;
--- functions/copy-and-push.test.js
+++ functions/copy-and-push.test.js
@@ -11,5 +11,13 @@ describe('copy and push function', () => {
     expect(newArray).toEqual(expectedArray);
   });
 
-  //   it('takes in an array and returns a copy with an additional item pushed into it', () => {});
+  it('takes in an array and returns a copy with an additional item pushed into it', () => {
+    const oldArray = ['Jeff', 'Abed', 'Annie'];
+    const newItem = 'Troy';
+    const newArray = copyAndPush(oldArray, newItem);
+
+    const expectedArray = ['Jeff', 'Abed', 'Annie', 'Troy'];
+
+    expect(newArray).toEqual(expectedArray);
+  });
 });
